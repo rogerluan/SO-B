e99aa32bc5b19da491ae0a583f52e0df1eac798b
Roger Oba <rogerluan.oba@gmail.com>
2017-10-31 15:22:11 -0200

    inverted the order of the function

--- proj2/minix/file.c
+++ proj2/minix/file.c
@@ -9,90 +9,6 @@
 #include "minix.h"
 #include "linux/uio.h"
 
-/**
- * generic_file_write_iter - write data to a file
- * @iocb:    IO state structure
- * @from:    iov_iter with data to write
- *
- * This is a wrapper around __generic_file_write_iter() to be used by most
- * filesystems. It takes care of syncing the file in case of O_SYNC file
- * and acquires i_mutex as needed.
- */
-ssize_t crypto_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
-{
-    struct file *file = iocb->ki_filp;
-    struct inode *inode = file->f_mapping->host;
-    ssize_t ret;
-    printk(KERN_INFO "Crypto: Customised print at %s\n", __FUNCTION__);
-
-    inode_lock(inode);
-    ret = generic_write_checks(iocb, from);
-    if (ret > 0)
-        ret = __generic_file_write_iter(iocb, from);
-    inode_unlock(inode);
-
-    if (ret > 0)
-        ret = generic_write_sync(iocb, ret);
-    return ret;
-}
-
-
-/**
- * generic_file_read_iter - generic filesystem read routine
- * @iocb:    kernel I/O control block
- * @iter:    destination for the data read
- *
- * This is the "read_iter()" routine for all filesystems
- * that can use the page cache directly.
- */
-ssize_t crypto_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
-{
-    struct file *file = iocb->ki_filp;
-    ssize_t retval = 0;
-    size_t count = iter->count;
-
-    printk(KERN_INFO "Crypto: Customised print at %s\n", __FUNCTION__);
-    if (!count)
-        goto out; /* skip atime */
-
-    if (iocb->ki_flags & IOCB_DIRECT) {
-        struct address_space *mapping = file->f_mapping;
-        struct inode *inode = mapping->host;
-        struct iov_iter data = *iter;
-        loff_t size;
-
-        size = i_size_read(inode);
-        retval = filemap_write_and_wait_range(mapping, iocb->ki_pos,
-                                              iocb->ki_pos + count - 1);
-        if (retval < 0)
-            goto out;
-
-        file_accessed(file);
-
-        retval = mapping->a_ops->direct_IO(iocb, &data);
-        if (retval >= 0) {
-            iocb->ki_pos += retval;
-            iov_iter_advance(iter, retval);
-        }
-
-        /*
-         * Btrfs can have a short DIO read if we encounter
-         * compressed extents, so if there was an error, or if
-         * we've already read everything we wanted to, or if
-         * there was a short read because we hit EOF, go ahead
-         * and return.  Otherwise fallthrough to buffered io for
-         * the rest of the read.  Buffered reads will not work for
-         * DAX files, so don't bother trying.
-         */
-        if (retval < 0 || !iter->count || iocb->ki_pos >= size ||
-            IS_DAX(inode))
-            goto out;
-    }
-
-    retval = do_generic_file_read(file, &iocb->ki_pos, iter, retval);
-out:
-    return retval;
-}
 
 /**
  * do_generic_file_read - generic file read routine
@@ -347,6 +263,92 @@ out:
     *ppos = ((loff_t)index << PAGE_SHIFT) + offset;
     file_accessed(filp);
     return written ? written : error;
+}
+
+
+/**
+ * generic_file_write_iter - write data to a file
+ * @iocb:    IO state structure
+ * @from:    iov_iter with data to write
+ *
+ * This is a wrapper around __generic_file_write_iter() to be used by most
+ * filesystems. It takes care of syncing the file in case of O_SYNC file
+ * and acquires i_mutex as needed.
+ */
+ssize_t crypto_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
+{
+    struct file *file = iocb->ki_filp;
+    struct inode *inode = file->f_mapping->host;
+    ssize_t ret;
+    printk(KERN_INFO "Crypto: Customised print at %s\n", __FUNCTION__);
+
+    inode_lock(inode);
+    ret = generic_write_checks(iocb, from);
+    if (ret > 0)
+        ret = __generic_file_write_iter(iocb, from);
+    inode_unlock(inode);
+
+    if (ret > 0)
+        ret = generic_write_sync(iocb, ret);
+    return ret;
+}
+
+
+/**
+ * generic_file_read_iter - generic filesystem read routine
+ * @iocb:    kernel I/O control block
+ * @iter:    destination for the data read
+ *
+ * This is the "read_iter()" routine for all filesystems
+ * that can use the page cache directly.
+ */
+ssize_t crypto_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
+{
+    struct file *file = iocb->ki_filp;
+    ssize_t retval = 0;
+    size_t count = iter->count;
+
+    printk(KERN_INFO "Crypto: Customised print at %s\n", __FUNCTION__);
+    if (!count)
+        goto out; /* skip atime */
+
+    if (iocb->ki_flags & IOCB_DIRECT) {
+        struct address_space *mapping = file->f_mapping;
+        struct inode *inode = mapping->host;
+        struct iov_iter data = *iter;
+        loff_t size;
+
+        size = i_size_read(inode);
+        retval = filemap_write_and_wait_range(mapping, iocb->ki_pos,
+                                              iocb->ki_pos + count - 1);
+        if (retval < 0)
+            goto out;
+
+        file_accessed(file);
+
+        retval = mapping->a_ops->direct_IO(iocb, &data);
+        if (retval >= 0) {
+            iocb->ki_pos += retval;
+            iov_iter_advance(iter, retval);
+        }
+
+        /*
+         * Btrfs can have a short DIO read if we encounter
+         * compressed extents, so if there was an error, or if
+         * we've already read everything we wanted to, or if
+         * there was a short read because we hit EOF, go ahead
+         * and return.  Otherwise fallthrough to buffered io for
+         * the rest of the read.  Buffered reads will not work for
+         * DAX files, so don't bother trying.
+         */
+        if (retval < 0 || !iter->count || iocb->ki_pos >= size ||
+            IS_DAX(inode))
+            goto out;
+    }
+
+    retval = do_generic_file_read(file, &iocb->ki_pos, iter, retval);
+out:
+    return retval;
 }
 
 /*
