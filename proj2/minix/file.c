646fb3fbde2a7101704a34486edb41f9a96f72e5
Roger Oba <rogerluan.oba@gmail.com>
2017-11-07 17:37:02 -0200

    implemented decipher

--- proj2/minix/file.c
+++ proj2/minix/file.c
@@ -45,14 +45,13 @@ static int bgmr_cipher(char *sentence, int encrypt);
  * filesystems. It takes care of syncing the file in case of O_SYNC file
  * and acquires i_mutex as needed.
  */
-ssize_t crypto_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
-{
+ssize_t crypto_file_write_iter(struct kiocb *iocb, struct iov_iter *from) {
     ssize_t len = from->iov->iov_len;
     char kernelBuffer[len];
     int errorCount = copy_from_user(kernelBuffer, from->iov->iov_base, len);
-    kernelBuffer[len]='\0';
+    kernelBuffer[len] = '\0';
     if (errorCount != 0) {
-        printk(KERN_INFO "CryptoDevice: Failed to receive %d characters from the user\n", errorCount);
+        Log("Failed to manipulate data. Error code: %d", errorCount);
         return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
     }
 
@@ -72,7 +71,7 @@ ssize_t crypto_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 
     errorCount = copy_to_user(from->iov->iov_base, message, strlen(message));
     if (errorCount != 0) {
-        printk(KERN_INFO "CryptoDevice: Failed to receive %d characters from the user\n", errorCount);
+        Log("Failed to manipulate data. Error code: %d", errorCount);
         return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
     }
 
@@ -124,48 +123,37 @@ ssize_t crypto_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
  * This is the "read_iter()" routine for all filesystems
  * that can use the page cache directly.
  */
-ssize_t crypto_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
-{
-    printk(KERN_INFO "Crypto: Customised print at %s\n", __FUNCTION__);
-//    if (!count)
-//        goto out; /* skip atime */
-//
-//    if (iocb->ki_flags & IOCB_DIRECT) {
-//        struct address_space *mapping = file->f_mapping;
-//        struct inode *inode = mapping->host;
-//        struct iov_iter data = *iter;
-//        loff_t size;
-//
-//        size = i_size_read(inode);
-//        retval = filemap_write_and_wait_range(mapping, iocb->ki_pos,
-//                                              iocb->ki_pos + count - 1);
-//        if (retval < 0)
-//            goto out;
-//
-//        file_accessed(file);
-//
-//        retval = mapping->a_ops->direct_IO(iocb, &data);
-//        if (retval >= 0) {
-//            iocb->ki_pos += retval;
-//            iov_iter_advance(iter, retval);
-//        }
-//
-//        /*
-//         * Btrfs can have a short DIO read if we encounter
-//         * compressed extents, so if there was an error, or if
-//         * we've already read everything we wanted to, or if
-//         * there was a short read because we hit EOF, go ahead
-//         * and return.  Otherwise fallthrough to buffered io for
-//         * the rest of the read.  Buffered reads will not work for
-//         * DAX files, so don't bother trying.
-//         */
-//        if (retval < 0 || !iter->count || iocb->ki_pos >= size ||
-//            IS_DAX(inode))
-//            goto out;
-//    }
-//
-//    retval = do_generic_file_read(file, &iocb->ki_pos, iter, retval);
-//out:
+ssize_t crypto_file_read_iter(struct kiocb *iocb, struct iov_iter *iter) {
+    ssize_t len = iter->iov->iov_len;
+    char kernelBuffer[len];
+    int errorCount = copy_from_user(kernelBuffer, iter->iov->iov_base, len);
+    kernelBuffer[len] = '\0';
+    if (errorCount != 0) {
+        Log("Failed to manipulate data. Error code: %d", errorCount);
+        return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
+    }
+
+    int i;
+    for (i = 0; i < BUFFER_SIZE ; i++) {
+        message[i] = '\0';
+    }
+
+    // Early quit if it detects data from text editor :thinking_face:
+    if (strcmp(kernelBuffer, "b0nano 2.5.3") == 0) {
+        return generic_file_write_iter(iocb, iter); // Implements the original function
+    }
+
+    Log("kernel buffer: %s", kernelBuffer);
+    bgmr_cipher(kernelBuffer, 0);
+
+
+    errorCount = copy_to_user(iter->iov->iov_base, message, strlen(message));
+    if (errorCount != 0) {
+        Log("Failed to manipulate data. Error code: %d", errorCount);
+        return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
+    }
+
+    Log("Deciphering and reading %ld bytes: \"%s\"", (long)len, iter->iov->iov_base);
     return generic_file_read_iter(iocb, iter); // Implements the original function
 }
 
